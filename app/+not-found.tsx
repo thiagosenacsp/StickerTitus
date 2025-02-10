4419272dcddfedec66b12a119f17bf34dc9df6e8
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2025-02-10 09:42:01 -0300

    chore: create not found screen

---
+++ app/+not-found.tsx
@@ -0,0 +1,30 @@
+import { View, StyleSheet } from "react-native";
+import { Link, Stack } from "expo-router";
+import React from "react";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! Not Found." }} />
+      <View style={styles.container}>
+        <Link href="/" style={styles.button}>
+          Go to Home screen.
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292E",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#FFF",
+  },
+});
