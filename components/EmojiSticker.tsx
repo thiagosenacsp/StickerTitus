3d034db614f866e9e0c11259107aaa593d9fe9e3
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2025-02-19 10:36:37 -0300

    chore: implement pan gesture

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,5 +1,4 @@
 import { type ImageSource } from "expo-image";
-import { View } from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -14,6 +13,8 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -32,15 +33,35 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     };
   });
 
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-          resizeMode="contain"
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+            resizeMode="contain"
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
